478b682714db393a19de87bbdfb42899f97c2651
jep <juepark42@gmail.com>
2026-01-19 21:56:27 +0900

    chore: eslint, prettier 세팅

--- README.md
+++ README.md
@@ -3,10 +3,18 @@
 - 배포 URL: [https://vercel.com/juepark42gmailcoms-projects/v0-board-crud-system](https://vercel.com/juepark42gmailcoms-projects/v0-board-crud-system)
 
 ## 개발 흐름
+
 - [V0로 초안 퍼블리싱](https://v0.app/chat/ttYU5YdmI1Q)
   - 리스트 페이지
   - 상세 페이지
   - 로그인 페이지
 
+## 세팅
+
+- V0 초안 기반 eslint, prettier등 세팅
+
 ## 누락된 기능 구현
+
 - Claude Code, Copilot 보조 도구로 활용
+
+1. 리스트 페이지 무한 로딩
---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    ignores: ['.next/', 'node_modules/'],
+  },
+  prettierConfig,
+)
